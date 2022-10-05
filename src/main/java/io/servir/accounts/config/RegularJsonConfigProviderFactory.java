fbe4ad63c870b9a176e47ad6ca8a212c6a9fbe1e
arhughes11 <arhughes@my.waketech.edu>
2022-10-05 09:59:30 -0400

    adding-code

---
+++ src/main/java/io/servir/accounts/config/RegularJsonConfigProviderFactory.java
@@ -0,0 +1,7 @@
+package io.servir.accounts.config;
+
+import org.keycloak.services.util.JsonConfigProviderFactory;
+
+public class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory {
+
+}
--- src/test/java/io/servir/accounts/AccountsApplicationTests.java
+++ src/test/java/io/servir/accounts/AccountsApplicationTests.java
@@ -3,7 +3,7 @@ package io.servir.accounts;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.cloud.config.enabled=false"})
 class AccountsApplicationTests {
 
     @Test
